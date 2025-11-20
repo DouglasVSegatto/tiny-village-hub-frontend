0069bcd7b575db99e14b6325851997fb2d4f754b
Segatto <78928583+DouglasVSegatto@users.noreply.github.com>
2025-11-19 22:34:14 -0700

    updated API_BASE_URL to enviroment key

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,7 +1,7 @@
 // src/services/AuthService.js
 
 // This should match your backend's running location
-const API_BASE_URL = 'http://localhost:8080/api/auth';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // --- Token Management Functions ---
 
